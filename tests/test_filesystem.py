6da1eeded90cf1be2d47caf2326c5c0298961859
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-13 19:40:18 -0400

    test: Add developer tests for directory checking in test_filesystem.py.

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -8,6 +8,22 @@ from hypothesis import given, strategies
 from chasten import filesystem
 
 
+def test_valid_directory() -> None:
+    """Confirm that a valid directory is found."""
+    directory_str = "./tests/"
+    directory = pathlib.Path(directory_str)
+    confirmation = filesystem.confirm_valid_directory(directory)
+    assert confirmation is True
+
+
+def test_invalid_directory() -> None:
+    """Confirm that a valid directory is found."""
+    directory_str = "./testsNOT/"
+    directory = pathlib.Path(directory_str)
+    confirmation = filesystem.confirm_valid_directory(directory)
+    assert confirmation is False
+
+
 @given(directory=strategies.builds(pathlib.Path))
 @pytest.mark.hypothesis
 def test_fuzz_confirm_valid_directory_using_builds(directory: pathlib.Path) -> None:
