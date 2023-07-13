39143a5b0f83d502c251c44f34ac1b1207ab9414
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-13 18:39:36 -0400

    feat: Add more tests and assertions to test_constants.py.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -15,26 +15,33 @@ def test_filesystem_constants():
     assert constants.humanreadable.No == "No"
 
 
-@given(directory=strategies.text())
+@given(directory=strategies.text(), yes=strategies.text(), no=strategies.text())
 @pytest.mark.hypothesis
-def test_fuzz_init(directory):
+def test_fuzz_init(directory, yes, no):
     """Use Hypothesis to confirm that initial value is set correctly."""
     fs = constants.Filesystem(directory)
     assert fs.Current_Directory == directory
+    hr = constants.Humanreadable(yes, no)
+    assert hr.Yes == yes
+    assert hr.No == no
 
 
-@given(fs=strategies.builds(constants.Filesystem))
+@given(fs=strategies.builds(constants.Filesystem), hr=strategies.builds(constants.Humanreadable))
 @pytest.mark.hypothesis
-def test_immutable(fs):
+def test_fuzz_immutable(fs, hr):
     """Use Hypothesis to confirm that attribute's value cannot be re-assigned."""
     with pytest.raises(FrozenInstanceError):
         fs.Current_Directory = "/new/path"
+    with pytest.raises(FrozenInstanceError):
+        hr.Yes = "YES"
+    with pytest.raises(FrozenInstanceError):
+        hr.No = "NO"
 
 
 @given(dir1=strategies.text(), dir2=strategies.text())
 @pytest.mark.hypothesis
-def test_distinct_filesystem(dir1, dir2):
-    """Use Hypothesis to confirm equality when the directory names are the same."""
+def test_fuzz_distinct(dir1, dir2):
+    """Use Hypothesis to confirm equality when the inputs names are the same."""
     fs1 = constants.Filesystem(dir1)
     fs2 = constants.Filesystem(dir2)
     if dir1 != dir2:
@@ -45,7 +52,7 @@ def test_distinct_filesystem(dir1, dir2):
 
 @given(directory=strategies.text())
 @pytest.mark.hypothesis
-def test_fuzz_filesystem_dataclass_equality(directory):
+def test_fuzz_dataclass_equality(directory):
     """Use Hypothesis to confirm that the same directory makes the same constant."""
     dir1 = directory
     dir2 = directory
