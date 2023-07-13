2a39fcb7f31f7219e0d8bd760c4b0aa1ddc4e2f1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-13 18:44:11 -0400

    refactor: Remove test cases from test_main.py if not needed.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,33 +1,16 @@
 """Pytest test suite for the main module."""
 
-import pathlib
 import typing
+from pathlib import Path
 
-from hypothesis import given
-from hypothesis import strategies as st
+import pytest
+from hypothesis import given, strategies
 
-import chasten.main
+from chasten import main
 
 
-@given(directory=st.from_type(pathlib.Path))
-def test_fuzz_confirm_valid_directory(directory: pathlib.Path) -> None:
+@given(directory=strategies.lists(strategies.builds(Path), min_size=1, max_size=5))
+@pytest.mark.hypothesis
+def test_fuzz_search(directory: typing.List[Path]) -> None:
     """Confirm that the function does not crash."""
-    chasten.main.confirm_valid_directory(directory=directory)
-
-
-@given(file=st.from_type(pathlib.Path))
-def test_fuzz_confirm_valid_file(file: pathlib.Path) -> None:
-    """Confirm that the function does not crash."""
-    chasten.main.confirm_valid_file(file=file)
-
-
-@given(answer=st.booleans())
-def test_fuzz_human_readable_boolean(answer: bool) -> None:
-    """Confirm that the function does not crash."""
-    chasten.main.human_readable_boolean(answer=answer)
-
-
-@given(directory=st.from_type(typing.List[pathlib.Path]))
-def test_fuzz_search(directory: typing.List[pathlib.Path]) -> None:
-    """Confirm that the function does not crash."""
-    chasten.main.search(directory=directory)
+    main.search(directory=directory)
