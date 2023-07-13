503df250a490a5bef4d524867606f1318bb0bab4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-13 19:53:38 -0400

    test: Add tests/test_util.py with developer-driven and hypothesis tests.

---
+++ tests/test_util.py
@@ -0,0 +1,30 @@
+"""Pytest test suite for the util module."""
+
+import pytest
+from hypothesis import given, strategies
+
+from chasten import util
+
+
+def test_human_readable_boolean() -> None:
+    """Use Hypothesis to confirm that the function does not crash."""
+    assert util.get_human_readable_boolean(answer=True) == "Yes"
+    assert util.get_human_readable_boolean(answer=False) == "No"
+
+
+@given(answer=strategies.booleans())
+@pytest.mark.fuzz
+def test_fuzz_human_readable_boolean(answer: bool) -> None:
+    """Use Hypothesis to confirm that the function does not crash."""
+    util.get_human_readable_boolean(answer=answer)
+
+
+@given(answer=strategies.booleans())
+@pytest.mark.fuzz
+def test_fuzz_human_readable_boolean_correct_string(answer: bool) -> None:
+    """Use Hypothesis to confirm that the conversion to human-readable works."""
+    str_answer = util.get_human_readable_boolean(answer=answer)
+    if answer:
+        assert str_answer == "Yes"
+    else:
+        assert str_answer == "No"
