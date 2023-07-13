89478af64a1f8c172f6b6f66f018c01b17691e70
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-13 18:40:01 -0400

    feat: Add more file and dir checks and new function to filesystem.py.

--- chasten/filesystem.py
+++ chasten/filesystem.py
@@ -1,6 +1,9 @@
 """Check and access contents of the filesystem."""
 
 from pathlib import Path
+from typing import List
+
+from chasten import constants
 
 
 def confirm_valid_file(file: Path) -> bool:
@@ -8,7 +11,7 @@ def confirm_valid_file(file: Path) -> bool:
     # determine if the file is not None and if it is a file
     if file is not None:
         # the file is valid
-        if file.is_file():
+        if file.is_file() and file.exists():
             return True
     # the file was either none or not valid
     return False
@@ -19,8 +22,13 @@ def confirm_valid_directory(directory: Path) -> bool:
     # determine if the file is not None and if it is a file
     if directory is not None:
         # the file is valid
-        if directory.is_dir():
+        if directory.is_dir() and directory.exists():
             return True
     # the directory was either none or not valid
     return False
 
+
+def get_default_directory_list() -> List[Path]:
+    """Return the default directory list that is the current working directory by itself."""
+    default_directory_list = [Path(constants.filesystem.Current_Directory)]
+    return default_directory_list
