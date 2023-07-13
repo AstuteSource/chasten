ca579bdbcf6a7e8873013236aef991ef9cc449a4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-13 19:42:37 -0400

    test: Add more tests to test_filesystem.py for file checking.

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -24,6 +24,22 @@ def test_invalid_directory() -> None:
     assert confirmation is False
 
 
+def test_valid_file() -> None:
+    """Confirm that a valid directory is found."""
+    file_str = "./tests/test_filesystem.py"
+    this_file = pathlib.Path(file_str)
+    confirmation = filesystem.confirm_valid_file(this_file)
+    assert confirmation is True
+
+
+def test_invalid_file() -> None:
+    """Confirm that a valid directory is found."""
+    file_str = "./tests/test_filesystemNOT.py"
+    this_file_not = pathlib.Path(file_str)
+    confirmation = filesystem.confirm_valid_file(this_file_not)
+    assert confirmation is False
+
+
 @given(directory=strategies.builds(pathlib.Path))
 @pytest.mark.hypothesis
 def test_fuzz_confirm_valid_directory_using_builds(directory: pathlib.Path) -> None:
