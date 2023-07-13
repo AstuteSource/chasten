eb941a0e9e1730b1fe29f3f0ef4d3c32ae6dea3d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-13 18:30:03 -0400

    refactor: Use a new approach for constant-definition in constants.py.

--- chasten/constants.py
+++ chasten/constants.py
@@ -1,30 +1,20 @@
-"""Define constants for use in chasten."""
+"""Define constants with dataclasses for use in chasten."""
 
-import collections
-import itertools
-
-
-def create_constants(name, *args, **kwargs):
-    """Create a namedtuple of constants."""
-    # the constants are created such that:
-    # the name is the name of the namedtuple
-    # for *args with "Constant_Name" or **kwargs with Constant_Name = "AnyConstantName"
-    # note that this creates a constant that will
-    # throw an AttributeError when attempting to redefine
-    new_constants = collections.namedtuple(name, itertools.chain(args, kwargs.keys()))
-    return new_constants(*itertools.chain(args, kwargs.values()))
+from dataclasses import dataclass
 
 
-# define the constants for file system access
-filesystem = create_constants(
-    "filesystem",
-    Current_Directory=".",
-)
+# filesystem constant
+@dataclass(frozen=True)
+class Filesystem:
+    Current_Directory: str
+
+filesystem = Filesystem(".")
 
 
-# define the constants for human-readable output
-humanreadable = create_constants(
-    "humanreadable",
-    Yes="Yes",
-    No="No",
-)
+# humanreadable constant
+@dataclass(frozen=True)
+class Humanreadable:
+    Yes: str
+    No: str
+
+humanreadable = Humanreadable("Yes", "No")
