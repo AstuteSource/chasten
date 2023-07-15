e1a7688280d844a3eff7bd49b04201f07225c072
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-15 11:55:09 -0400

    feat: Add the display argument for the configure command in main.py.

--- chasten/main.py
+++ chasten/main.py
@@ -1,5 +1,6 @@
 """Chasten checks the AST of a Python program."""
 
+from enum import Enum
 from pathlib import Path
 from typing import List
 
@@ -7,13 +8,25 @@ import typer
 from platformdirs import user_config_dir
 from pyastgrep import search as pyastgrepsearch  # type: ignore
 from rich.console import Console
+from rich.tree import Tree
 from trogon import Trogon  # type: ignore
 from typer.main import get_group
 
-from chasten import constants, debug, filesystem, output, server
+from chasten import constants
+from chasten import debug
+from chasten import filesystem
+from chasten import output
+from chasten import server
 
 # create a Typer object to support the command-line interface
 cli = typer.Typer()
+
+
+class ConfigureTask(str, Enum):
+    """Define the different task possibilities."""
+
+    CREATE = "create"
+    DISPLAY = "display"
 
 
 @cli.command()
@@ -24,9 +37,10 @@ def interact(ctx: typer.Context) -> None:
 
 @cli.command()
 def configure(
-    create: bool = typer.Option(
-        False, "--create", "-c", help="Create platform-specific configuration directory"
-    ),
+    task: ConfigureTask = typer.Argument(ConfigureTask.DISPLAY.value),
+    # create: bool = typer.Option(
+    #     False, "--create", "-c", help="Create platform-specific configuration directory"
+    # ),
     verbose: bool = typer.Option(False),
     debug_level: debug.DebugLevel = typer.Option(debug.DebugLevel.ERROR.value),
     debug_destination: debug.DebugDestination = typer.Option(
@@ -36,25 +50,44 @@ def configure(
     """Create a configuration."""
     # setup the console and the logger through output module
     output.setup(debug_level, debug_destination)
-    output.logger.debug(f"Create a configuration? {create}")
+    output.logger.debug(f"Task? {task}")
     output.logger.debug(f"Display verbose output? {verbose}")
+    output.logger.debug(f"Debug level? {debug_level.value}")
+    output.logger.debug(f"Debug destination? {debug_destination.value}")
     # display the header
     output.print_header()
     # display details about configuration as
     # long as verbose output was requested
     output.print_diagnostics(
         verbose,
+        task=task,
         debug_level=debug_level,
         debug_destination=debug_destination,
     )
-    # create the configuration directory
-    if create:
+    # display the configuration directory and its contents
+    if task == ConfigureTask.DISPLAY:
+        output.console.print(":sparkles: Configuration directory:" + constants.markers.Newline)
         chasten_user_config_dir_str = user_config_dir(
             appname=constants.chasten.Application_Name,
             appauthor=constants.chasten.Application_Author,
         )
         chasten_user_config_dir_path = Path(chasten_user_config_dir_str)
-        chasten_user_config_dir_path.mkdir(parents=True)
+        tree = Tree(
+            f":open_file_folder: [link file://{chasten_user_config_dir_str}]{chasten_user_config_dir_str}",
+            guide_style="bold bright_blue",
+        )
+        rich_path_tree = filesystem.create_directory_tree(chasten_user_config_dir_path)
+        output.console.print(rich_path_tree)
+        output.console.print()
+
+    # # create the configuration directory
+    # if create:
+    #     chasten_user_config_dir_str = user_config_dir(
+    #         appname=constants.chasten.Application_Name,
+    #         appauthor=constants.chasten.Application_Author,
+    #     )
+    #     chasten_user_config_dir_path = Path(chasten_user_config_dir_str)
+    #     chasten_user_config_dir_path.mkdir(parents=True)
 
 
 @cli.command()
