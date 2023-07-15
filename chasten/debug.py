7c4bb50dfb03cbe246ec30e6a753e4eeaaf76811
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-15 10:02:48 -0400

    feat: Add the debug.pu file with DebugLevel and DebugDestination enum classes.

---
+++ chasten/debug.py
@@ -0,0 +1,20 @@
+"""Support and configure debugging."""
+
+from enum import Enum
+
+
+class DebugLevel(str, Enum):
+    """The predefined levels for debugging."""
+
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+
+class DebugDestination(str, Enum):
+    """The destination for debugging."""
+
+    CONSOLE = "console"
+    SYSLOG = "syslog"
