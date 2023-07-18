9530930b0c3b6622b0611bd4a798f403343d8c8a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-18 12:48:12 -0400

    test: Add more assertions and checks to test_debug.py.

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -6,6 +6,7 @@ from chasten.debug import DebugDestination, DebugLevel
 
 
 def test_debug_level_values():
+    """Confirm that all of the enumeration values are correct."""
     assert DebugLevel.DEBUG == "DEBUG"
     assert DebugLevel.INFO == "INFO"
     assert DebugLevel.WARNING == "WARNING"
@@ -14,10 +15,16 @@ def test_debug_level_values():
 
 
 def test_debug_level_isinstance():
+    """Confirm that all of the individual levels are of the correct type."""
     assert isinstance(DebugLevel.DEBUG, DebugLevel)
+    assert isinstance(DebugLevel.INFO, DebugLevel)
+    assert isinstance(DebugLevel.WARNING, DebugLevel)
+    assert isinstance(DebugLevel.ERROR, DebugLevel)
+    assert isinstance(DebugLevel.CRITICAL, DebugLevel)
 
 
 def test_debug_level_iteration():
+    """Confirm that it is possible to list all of the possible values"""
     assert list(DebugLevel) == ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
 
