9aa46c9d933259967e39f3424c46468d6306c2c9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-18 10:22:54 -0400

    refactor: Revise validate.py so that it uses option for parameters and the standard validate function in validate_configuration.

--- chasten/validate.py
+++ chasten/validate.py
@@ -1,30 +1,37 @@
 """Validate various aspects of configurations and command-line arguments."""
 
-from typing import Tuple
+from typing import Any, Dict, Tuple
 
-from jsonschema import Draft7Validator
+import jsonschema
 from jsonschema.exceptions import ValidationError
 
 from chasten import constants
 
 JSON_SCHEMA = {
     "type": "object",
+    "required": [],
     "properties": {
         "chasten": {
             "type": "object",
             "properties": {
-                "verbose": {"type": "boolean"},
+                "verbose": {
+                    "type": "boolean",
+                    "required": [],
+                },
                 "debug-level": {
                     "type": "string",
                     "enum": ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+                    "required": [],
                 },
                 "debug-destination": {
                     "type": "string",
                     "enum": ["CONSOLE", "SYSLOG"],
+                    "required": [],
                 },
                 "search-directory": {
                     "type": "array",
                     "items": {"type": "string"},
+                    "required": [],
                 },
             },
             "additionalProperties": False,
@@ -33,13 +40,14 @@ JSON_SCHEMA = {
 }
 
 
-def validate_configuration(configuration) -> Tuple[bool, str]:
+def validate_configuration(
+    configuration: Dict[str, Dict[str, Any]]
+) -> Tuple[bool, str]:
     """Validate the configuration."""
     # indicate that validation passed; since there
     # were no validation errors, return an empty string
     try:
-        validator = Draft7Validator(JSON_SCHEMA)
-        validator.validate(configuration, JSON_SCHEMA)
+        jsonschema.validate(configuration, JSON_SCHEMA)
         return (True, constants.markers.Empty)
     # indicate that validation failed;
     # since validation errors exist, package them up
@@ -47,4 +55,5 @@ def validate_configuration(configuration) -> Tuple[bool, str]:
     except ValidationError as validation_error:
         error_message = str(validation_error)
         error_message = error_message.lstrip()
+        print(error_message)
         return (False, error_message)
