749470a4aae9a6833007e3cb9b1516509604ef7c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-18 12:54:31 -0400

    test: Add more tests and assertions to test_debug.py.

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -24,19 +24,34 @@ def test_debug_level_isinstance():
 
 
 def test_debug_level_iteration():
-    """Confirm that it is possible to list all of the possible values"""
+    """Confirm that it is possible to list all of the possible values."""
     assert list(DebugLevel) == ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
 
 def test_debug_destination_values():
+    """Confirm that all of the enumeration values are correct."""
     assert DebugDestination.CONSOLE == "CONSOLE"
     assert DebugDestination.SYSLOG == "SYSLOG"
 
 
+def test_debug_destination_isinstance():
+    """Confirm that all of the individual levels are of the correct type."""
+    assert isinstance(DebugDestination.CONSOLE, DebugDestination)
+    assert isinstance(DebugDestination.SYSLOG, DebugDestination)
+
+
 def test_debug_destination_iteration():
+    """Confirm that it is possible to list all of the possible valuers."""
     assert list(DebugDestination) == ["CONSOLE", "SYSLOG"]
 
 
+def test_level_destination_invalid():
+    """Confirm that invalid values raise a ValueError."""
+    with pytest.raises(ValueError):
+        DebugLevel("INVALID")
+
+
 def test_debug_destination_invalid():
+    """Confirm that invalid values raise a ValueError."""
     with pytest.raises(ValueError):
         DebugDestination("INVALID")
