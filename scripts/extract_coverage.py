ef30a0bf29a4254be329ca1746a25a29416de8a0
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-20 10:23:02 -0400

    chore: Add pragmas to ignore checks in extract_coverage.py.

--- scripts/extract_coverage.py
+++ scripts/extract_coverage.py
@@ -1,14 +1,16 @@
+"""Demonstrate how to extract coverage with coverage.py."""
+
 from coverage import CoverageData
 
 # Construct CoverageData instance
 data = CoverageData()
 data.read()
 
-print(data)
+print(data)  # noqa
 
 # Get covered line numbers for a file
-filename = 'chasten/util.py'
-covered_lines = set(data.lines(filename))
+filename = "chasten/util.py"
+covered_lines = set(data.lines(filename))  # type: ignore
 
-print(f"Covered lines in {filename}:")
-print(covered_lines)
+print(f"Covered lines in {filename}:")   # noqa
+print(covered_lines)  # noqa
