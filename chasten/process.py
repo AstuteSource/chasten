64b0d440e8a215f1d6db1f74daab2b67da96374b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-22 21:29:41 -0400

    fix: Correct defects in include_or_exclude_checks and add features to it as well in process.py.

--- chasten/process.py
+++ chasten/process.py
@@ -8,31 +8,38 @@ from thefuzz import fuzz
 from chasten import constants, enumerations
 
 
-def include_checks(
+def include_or_exclude_checks(
     checks: List[Dict[str, Union[str, Dict[str, int]]]],
-    check_include_attribute: enumerations.FilterableAttribute,
-    check_include_match: str,
-    check_include_confidence: int = constants.checks.Check_Confidence,
+    check_attribute: enumerations.FilterableAttribute,
+    check_match: str,
+    check_confidence: int = constants.checks.Check_Confidence,
+    include: bool = True,
 ) -> List[Dict[str, Union[str, Dict[str, int]]]]:
     """Perform all of the includes and excludes for the list of checks."""
     filtered_checks = []
     # at least one aspect of the inputs was not specified (likely due to
     # the fact that the command-line argument(s) were not used) and thus
     # there is no filtering that should take place; return the input
-    if not check_include_attribute or not check_include_match:
-        return filtered_checks
+    if check_attribute is None or check_match is None:
+        return checks
     # the function's inputs are valid and so perform the filtering
     for check in checks:
         # extract the contents of the requested attribute for inclusion
-        check_requested_include_attribute = check[check_include_attribute]
+        check_requested_include_attribute = check[check_attribute]
         # compute the fuzzy match value for the specific:
         # --> requested include attribute
         # --> specified match string
-        fuzzy_include_value = fuzz.ratio(
-            check_include_match, check_requested_include_attribute
+        fuzzy_value = fuzz.ratio(
+            check_match, check_requested_include_attribute
         )
-        # include the check if the fuzzy inclusion value is above the default threshold
-        if fuzzy_include_value >= check_include_confidence:
+        # include the check if the fuzzy inclusion value is above (or equal to) threshold
+        # and the purpose of the function call is to include values
+        if (fuzzy_value >= check_confidence) and include:
+            filtered_checks.append(check)
+        # include the check if the fuzzy inclusion value is below threshold
+        # and the purpose of the function call is to exclude values;
+        # note that not including a value means that it excludes
+        elif (fuzzy_value < check_confidence) and not include:
             filtered_checks.append(check)
     return filtered_checks
 
