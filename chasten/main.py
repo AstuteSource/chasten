e919e5cf38f75e92915ed66dc704bf204961cc79
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-22 21:27:00 -0400

    feat: Add the call to include_or_exclude_checks twice in main.py.

--- chasten/main.py
+++ chasten/main.py
@@ -295,7 +295,9 @@ def analyze(  # noqa: PLR0913
     check_list: List[Dict[str, Union[str, Dict[str, int]]]] = checks_dict[constants.checks.Checks_Label]
     # filter the list of checks based on the include and exclude parameters
     # --> only run those checks that were included
-    check_list = process.include_checks(check_list, check_include[0], check_include[1], check_include[2])
+    check_list = process.include_or_exclude_checks(check_list, check_include[0], check_include[1], check_include[2], include=True)
+    # --> remove those checks that were excluded
+    check_list = process.include_or_exclude_checks(check_list, check_exclude[0], check_exclude[1], check_exclude[2], include=False)
     # collect all of the directories that are invalid
     invalid_directories = []
     for current_directory in directory:
