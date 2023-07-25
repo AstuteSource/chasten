47cbdad915cae4656c944438ea881bd59a1df516
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-25 17:40:04 -0400

    style: Fix formatting mistakes in main.py.

--- chasten/main.py
+++ chasten/main.py
@@ -2,11 +2,7 @@
 
 import sys
 from pathlib import Path
-from typing import Any
-from typing import Dict
-from typing import List
-from typing import Tuple
-from typing import Union
+from typing import Any, Dict, List, Tuple, Union
 
 import typer
 import yaml
@@ -16,16 +12,18 @@ from rich.syntax import Syntax
 from trogon import Trogon  # type: ignore
 from typer.main import get_group
 
-from chasten import configuration
-from chasten import constants
-from chasten import debug
-from chasten import enumerations
-from chasten import filesystem
-from chasten import output
-from chasten import process
-from chasten import server
-from chasten import util
-from chasten import validate
+from chasten import (
+    configuration,
+    constants,
+    debug,
+    enumerations,
+    filesystem,
+    output,
+    process,
+    server,
+    util,
+    validate,
+)
 
 # create a Typer object to support the command-line interface
 cli = typer.Typer()
