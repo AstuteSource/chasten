9cddb8d25ec1b24ea13c59e435943cdee570f225
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-27 12:41:16 -0400

    feat: Add functions to checks.py for creating attribute labels.

--- chasten/checks.py
+++ chasten/checks.py
@@ -1,7 +1,6 @@
 """Extract and analyze details about specific checks."""
 
-
-from typing import Dict, Tuple, Union
+from typing import Dict, List, Tuple, Union
 
 from chasten import constants
 
@@ -15,3 +14,29 @@ def extract_min_max(
     min_count = check.get(constants.checks.Check_Count, {}).get(constants.checks.Check_Min)  # type: ignore
     max_count = check.get("count", {}).get("max")  # type: ignore
     return (min_count, max_count)
+
+
+def create_attribute_label(attribute: Union[str, int, None], label: str) -> str:
+    """Create an attribute label string for display as long as it is not null."""
+    # define an empty attribute string, which is
+    # the default in the case when attribute is None
+    labeled_attribute = ""
+    # the attribute is not None and thus the function
+    # should create the labelled attribute out of it
+    if attribute:
+        labeled_attribute = f"{label} = {attribute}"
+    return labeled_attribute
+
+
+def join_attribute_labels(attribute_labels: List[str]) -> str:
+    """Join all of the attribute labels in a comma-separated list."""
+    joined_attribute_labels = constants.markers.Empty_String
+    # incrementally create the list of labelled attributes,
+    # separating each one with a comma and a space and
+    # ensuring that the last one does not have a trailing
+    # comma and space after it
+    for i, attribute_label in enumerate(attribute_labels):
+        if i > 0:
+            joined_attribute_labels += constants.markers.Comma_Space
+        joined_attribute_labels += attribute_label  # type: ignore
+    return joined_attribute_labels
