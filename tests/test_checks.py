f9a91b2fbc26b47275bc4423a67bd4c2b8fb7b92
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-27 12:41:53 -0400

    style: Fix formatting and comments in the test_checks.py.

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -18,13 +18,7 @@ def test_extract_min_max_hypothesis(check):
 
 def test_extract_min_max():
     """Confirm that it is possible to extract both values from the count parmeter when it exists."""
-    check = {
-        "name": "test",
-        "count": {
-            "min": 1,
-            "max": 10
-        }
-    }
+    check = {"name": "test", "count": {"min": 1, "max": 10}}
     min_count, max_count = extract_min_max(check)
     assert min_count == 1
     assert max_count == 10  # noqa
@@ -32,12 +26,7 @@ def test_extract_min_max():
 
 def test_extract_max():
     """Confirm that it is possible to extract one value from the count parmeter when it exists."""
-    check = {
-        "name": "test",
-        "count": {
-            "max": 10
-        }
-    }
+    check = {"name": "test", "count": {"max": 10}}
     min_count, max_count = extract_min_max(check)
     assert min_count is None
     assert max_count == 10  # noqa
@@ -49,19 +38,16 @@ def test_extract_min():
         "name": "test",
         "count": {
             "min": 1,
-        }
+        },
     }
     min_count, max_count = extract_min_max(check)
     assert min_count == 1
     assert max_count is None
 
 
-
 def test_extract_min_max_missing():
     """Confirm that it is not possible to extract both values when they do not exist."""
-    check = {
-        "name": "test"
-    }
+    check = {"name": "test"}
     min_count, max_count = extract_min_max(check)  # type: ignore
     assert min_count is None
     assert max_count is None
