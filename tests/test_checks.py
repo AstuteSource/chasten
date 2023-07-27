06e335598ef5f58f069b97a32b18b6c068b3c2f9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-07-27 11:53:40 -0400

    test: Add more tests for a single parameter extraction in test_checks.py.

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -17,6 +17,33 @@ def test_extract_min_max():
     assert max_count == 10  # noqa
 
 
+def test_extract_max():
+    """Confirm that it is possible to extract one value from the count parmeter when it exists."""
+    check = {
+        "name": "test",
+        "count": {
+            "max": 10
+        }
+    }
+    min_count, max_count = extract_min_max(check)
+    assert min_count is None
+    assert max_count == 10  # noqa
+
+
+def test_extract_min():
+    """Confirm that it is possible to extract one value from the count parmeter when it exists."""
+    check = {
+        "name": "test",
+        "count": {
+            "min": 1,
+        }
+    }
+    min_count, max_count = extract_min_max(check)
+    assert min_count == 1
+    assert max_count is None
+
+
+
 def test_extract_min_max_missing():
     """Confirm that it is not possible to extract both values when they do not exist."""
     check = {
