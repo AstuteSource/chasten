ff8f2c9537950c589b9a7cf71a5e2e12b842806e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-08-05 11:21:35 -0400

    fix: Add a reanalyze function that will allow for complete refactorting in main.py.

--- chasten/main.py
+++ chasten/main.py
@@ -346,6 +346,311 @@ def configure(  # noqa: PLR0913
 
 
 @cli.command()
+def reanalyze(  # noqa: PLR0913, PLR0915
+    project: str = typer.Option(
+        ..., "--project-name", "-p", help="Name of the project."
+    ),
+    check_include: Tuple[enumerations.FilterableAttribute, str, int] = typer.Option(
+        (None, None, 0),
+        "--check-include",
+        "-i",
+        help="Attribute name, value, and match confidence level for inclusion.",
+    ),
+    check_exclude: Tuple[enumerations.FilterableAttribute, str, int] = typer.Option(
+        (None, None, 0),
+        "--check-exclude",
+        "-e",
+        help="Attribute name, value, and match confidence level for exclusion.",
+    ),
+    input_path: Path = typer.Option(
+        filesystem.get_default_directory_list(),
+        "--search-path",
+        "-d",
+        help="A path (i.e., directory or file) with Python source code(s).",
+        exists=True,
+        file_okay=True,
+        dir_okay=True,
+        readable=True,
+        resolve_path=True,
+    ),
+    output_directory: Path = typer.Option(
+        None,
+        "--save-directory",
+        "-s",
+        help="A directory for saving output file(s).",
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        readable=True,
+        writable=True,
+        resolve_path=True,
+    ),
+    config: Path = typer.Option(
+        None,
+        "--config",
+        "-c",
+        help="A directory with configuration file(s).",
+    ),
+    debug_level: debug.DebugLevel = typer.Option(
+        debug.DebugLevel.ERROR.value,
+        "--debug-level",
+        "-l",
+        help="Specify the level of debugging output.",
+    ),
+    debug_destination: debug.DebugDestination = typer.Option(
+        debug.DebugDestination.CONSOLE.value,
+        "--debug-dest",
+        "-t",
+        help="Specify the destination for debugging output.",
+    ),
+    verbose: bool = typer.Option(False, help="Enable verbose mode output."),
+    save: bool = typer.Option(False, help="Enable saving of output file(s)."),
+) -> None:
+    """💫 Analyze the AST of Python source code."""
+    # output the preamble, including extra parameters specific to this function
+    output_preamble(
+        verbose,
+        debug_level,
+        debug_destination,
+        project=project,
+        directory=input_path,
+    )
+    # create the include and exclude criteria
+    revised_check_include = checks.fix_check_criteria(check_include)
+    include = results.CheckCriterion(
+        attribute=revised_check_include[0],
+        value=revised_check_include[1],
+        confidence=revised_check_include[2],
+    )
+    revised_check_exclude = checks.fix_check_criteria(check_exclude)
+    exclude = results.CheckCriterion(
+        attribute=revised_check_exclude[0],
+        value=revised_check_exclude[1],
+        confidence=revised_check_exclude[2],
+    )
+    # create and store a configuration object for the result
+    chasten_configuration = results.Configuration(
+        projectname=project,
+        configdirectory=config,
+        searchpath=input_path,
+        debuglevel=debug_level,
+        debugdestination=debug_destination,
+        checkinclude=include,
+        checkexclude=exclude,
+    )
+    chasten_results_save = results.Chasten(configuration=chasten_configuration)
+    # add extra space after the command to run the program
+    output.console.print()
+    # validate the configuration
+    (validated, checks_dict) = validate_configuration_files(config, verbose)
+    # some aspect of the configuration was not
+    # valid, so exit early and signal an error
+    if not validated:
+        output.console.print(
+            "\n:person_shrugging: Cannot perform analysis due to configuration error(s).\n"
+        )
+        sys.exit(constants.markers.Non_Zero_Exit)
+    # extract the list of the specific patterns (i.e., the XPATH expressions)
+    # that will be used to analyze all of the XML-based representations of
+    # the Python source code found in the valid directories
+    check_list: List[Dict[str, Union[str, Dict[str, int]]]] = checks_dict[
+        constants.checks.Checks_Label
+    ]
+    # filter the list of checks based on the include and exclude parameters
+    # --> only run those checks that were included
+    check_list = process.include_or_exclude_checks(  # type: ignore
+        check_list, include=True, *check_include
+    )
+    # --> remove those checks that were excluded
+    check_list = process.include_or_exclude_checks(  # type: ignore
+        check_list, include=False, *check_exclude
+    )
+    # the specified search path is not valid and thus it is
+    # not possible to analyze the Python source files in this directory
+    # OR
+    # the specified search path is not valid and thus it is
+    # not possible to analyze the specific Python source code file
+    if not filesystem.confirm_valid_directory(
+        input_path
+    ) and not filesystem.confirm_valid_file(input_path):
+        output.console.print(
+            "\n:person_shrugging: Cannot perform analysis due to invalid search directory.\n"
+        )
+        sys.exit(constants.markers.Non_Zero_Exit)
+    # create the list of directories
+    valid_directories = [input_path]
+    # output the list of directories subject to checking
+    output.console.print(f":sparkles: Analyzing Python source code in:\n{input_path}")
+    # output the number of checks that will be performed
+    output.console.print()
+    output.console.print(f":tada: Running a total of {len(check_list)} check(s):")
+    # create a check_status list for all of the checks
+    check_status_list: List[bool] = []
+    # iterate through and perform each of the checks
+    for current_check in check_list:
+        # extract the pattern for the current check
+        current_xpath_pattern = str(current_check[constants.checks.Check_Pattern])  # type: ignore
+        # display the XPATH expression for the current check
+        output.console.print("\n:tada: Performing check:")
+        xpath_syntax = Syntax(
+            current_xpath_pattern,
+            constants.markers.Xml,
+            theme=constants.chasten.Theme_Colors,
+        )
+        # extract the minimum and maximum values for the checks, if they exist
+        # note that this function will return None for a min or a max if
+        # that attribute does not exist inside of the current_check; importantly,
+        # having a count or a min or a max is all optional in a checks file
+        (min_count, max_count) = checks.extract_min_max(current_check)
+        min_label = checks.create_attribute_label(min_count, constants.checks.Check_Min)
+        max_label = checks.create_attribute_label(max_count, constants.checks.Check_Max)
+        # extract details about the check to display in the header
+        # of the syntax box for this specific check
+        check_id = current_check[constants.checks.Check_Id]  # type: ignore
+        check_id_label = checks.create_attribute_label(check_id, constants.checks.Check_Id)  # type: ignore
+        check_name = current_check[constants.checks.Check_Name]  # type: ignore
+        check_name_label = checks.create_attribute_label(check_name, constants.checks.Check_Name)  # type: ignore
+        # create the combined attribute label that displays all details for the check
+        combined_attribute_label = checks.join_attribute_labels(
+            [check_id_label, check_name_label, min_label, max_label]
+        )
+        # display the check with additional details about its configuration
+        output.console.print(
+            Panel(
+                xpath_syntax,
+                expand=False,
+                title=f"{combined_attribute_label}",
+            )
+        )
+        # search for the XML contents of an AST that match the provided
+        # XPATH query using the search_python_file in search module of pyastgrep
+        match_generator = pyastgrepsearch.search_python_files(
+            paths=valid_directories, expression=current_xpath_pattern, xpath2=True
+        )
+        # materialize a list from the generator of (potential) matches;
+        # note that this list will also contain an object that will
+        # indicate that the analysis completed for each located file
+        match_generator_list = list(match_generator)
+        # filter the list of matches so that it only includes
+        # those that are a Match object that will contain source code
+        (match_generator_list, _) = process.filter_matches(
+            match_generator_list, pyastgrepsearch.Match
+        )
+        output.console.print()
+        output.console.print(
+            f":sparkles: Found a total of {len(match_generator_list)} matches"
+        )
+        # perform an enforceable check if it is warranted for this check
+        current_check_save = None
+        if checks.is_checkable(min_count, max_count):
+            # determine whether or not the number of found matches is within mix and max
+            check_status = checks.check_match_count(
+                len(match_generator_list), min_count, max_count
+            )
+            # produce and display a status message about the check
+            check_status_message = checks.make_checks_status_message(check_status)
+            output.console.print(check_status_message)
+            # keep track of the outcome for this check
+            check_status_list.append(check_status)
+            # create the current check
+            current_check_save = results.Check(
+                id=check_id,  # type: ignore
+                name=check_name,  # type: ignore
+                min=min_count,  # type: ignore
+                max=max_count,  # type: ignore
+                pattern=current_xpath_pattern,
+                passed=check_status,
+            )
+        # for each potential match, log and, if verbose model is enabled,
+        # display details about each of the matches
+        current_result_source = results.Source(
+            name=str([str(vd) for vd in valid_directories])
+        )
+        if len(match_generator_list) == 0:
+            current_result_source.results.append(current_check_save)  # type: ignore
+        current_match_sources_dict: Dict[str, results.Source] = {}
+        for search_output in match_generator_list:
+            current_result_source = results.Source(name=str(search_output.path))
+            current_match_sources_dict[str(search_output.path)] = current_result_source
+            if isinstance(search_output, pyastgrepsearch.Match):
+                # display a label for matching output information
+                output.opt_print_log(verbose, blank=constants.markers.Empty_String)
+                output.opt_print_log(verbose, label=":sparkles: Matching source code:")
+                # extract the direct line number for this match
+                position_end = search_output.position.lineno
+                # extract the column offset for this match
+                column_offset = search_output.position.col_offset
+                # get a pre-defined number of the lines both
+                # before and after the line that is the closest match;
+                # note that the use of "*" is an indicator of the
+                # specific line that is the focus of the search
+                all_lines = search_output.file_lines
+                # create a deepcopy of the listing of lines so that
+                # the annotated version of the lines for this specific
+                # match does not appear in annotated version of other matches
+                all_lines_for_marking = deepcopy(all_lines)
+                lines = all_lines_for_marking[
+                    max(0, position_end - constants.markers.Code_Context) : position_end
+                    + constants.markers.Code_Context
+                ]
+                # create a rich panel to display the results:
+                # key features:
+                # --> descriptive label
+                # --> syntax highlighting
+                # --> line numbers
+                # --> highlight for the matching position
+                # --> suitable theme (could be customized)
+                code_syntax = Syntax(
+                    "\n".join(str(line) for line in lines),
+                    constants.chasten.Programming_Language,
+                    theme=constants.chasten.Theme_Colors,
+                    background_color=constants.chasten.Theme_Background,
+                    line_numbers=True,
+                    start_line=(
+                        max(1, position_end - constants.markers.Code_Context + 1)
+                    ),
+                    highlight_lines={position_end},
+                )
+                # display the results in a rich panel
+                output.opt_print_log(
+                    verbose,
+                    panel=Panel(
+                        code_syntax,
+                        expand=False,
+                        title=f"{search_output.path}:{position_end}:{column_offset}",
+                    ),
+                )
+                # create a match and attach it to the current Check for saving
+                current_match_for_current_check_save = results.Match(
+                    lineno=position_end, coloffset=column_offset
+                )
+                current_check_save.matches.append(current_match_for_current_check_save)  # type: ignore
+                current_result_source.results.append(current_check_save)  # type: ignore
+            else:
+                current_result_source.results.append(current_check_save)  # type: ignore
+        output.console.print(current_match_sources_dict)
+        # output.console.print(list(current_match_sources_dict.values()))
+        # chasten_results_save.sources.append(current_result_source)
+        chasten_results_save.sources.extend(list(current_match_sources_dict.values()))
+    filesystem.write_results(
+        output_directory,
+        project
+        + constants.filesystem.Dash
+        + constants.filesystem.Main_Results_File_Name
+        + constants.filesystem.Dash,
+        chasten_results_save,
+    )
+    # confirm whether or not all of the checks passed
+    # and then display the appropriate diagnostic message
+    all_checks_passed = all(check_status_list)
+    if not all_checks_passed:
+        output.console.print("\n:sweat: At least one check did not pass.")
+        sys.exit(constants.markers.Non_Zero_Exit)
+    output.console.print("\n:joy: All checks passed.")
+
+
+
+@cli.command()
 def analyze(  # noqa: PLR0913, PLR0915
     project: str = typer.Option(
         ..., "--project-name", "-p", help="Name of the project."
