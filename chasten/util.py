f14fcbebe927fca1b91995125fc641ad514beb31
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-08-05 16:57:39 -0400

    feat: Add another function to display booleans in util.py.

--- chasten/util.py
+++ chasten/util.py
@@ -2,6 +2,9 @@
 
 from chasten import constants
 
+checkmark_unicode = "\u2713"
+xmark_unicode = "\u2717"
+
 
 def get_human_readable_boolean(answer: bool) -> str:
     """Produce a human-readable Yes or No for a boolean value of True or False."""
@@ -10,3 +13,10 @@ def get_human_readable_boolean(answer: bool) -> str:
         return constants.humanreadable.Yes
     # the provided answer is false
     return constants.humanreadable.No
+
+
+def get_symbol_boolean(answer: bool) -> str:
+    """Produce a symbol-formatted version of a boolean value of True or False."""
+    if answer:
+        return f"[green]{checkmark_unicode}[/green]"
+    return f"[red]{xmark_unicode}[/red]"
