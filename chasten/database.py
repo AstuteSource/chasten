abe485c28cabd4d578a85b187ae89e1a5b6b5ba3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2023-08-17 16:52:20 -0400

    refactor: Avoid some code duplication in database.py.

--- chasten/database.py
+++ chasten/database.py
@@ -7,7 +7,8 @@ from pathlib import Path
 
 from sqlite_utils import Database
 
-from chasten import constants, output
+from chasten import constants
+from chasten import output
 
 CHASTEN_SQL_SELECT_QUERY = """
 SELECT
@@ -83,6 +84,43 @@ def enable_full_text_search(chasten_database_name: str) -> None:
     # full-text search on the view called chasten_complete
 
 
+def display_datasette_details(
+    label: str,
+    virtual_env_location: str,
+    executable_path: str,
+    full_executable_name: str,
+    publish: bool
+) -> None:
+    """Display details about the current datasette configuration."""
+    # output diagnostic information about the datasette instance; note
+    # that the output must appear here and not from the calling function
+    # because once the datasette instance starts the chasten tool can
+    # no longer produce output in the console
+    output.console.print()
+    output.console.print(label)
+    output.console.print(
+        f"{constants.markers.Indent}{small_bullet_unicode} Venv: '{output.shorten_file_name(str(virtual_env_location), 120)}'"
+    )
+    if executable_path:
+        output.console.print(
+            f"{constants.markers.Indent}{small_bullet_unicode} Program: '{output.shorten_file_name(executable_path, 120)}'"
+        )
+    else:
+        output.console.print(
+            f"{constants.markers.Indent}{small_bullet_unicode} Cannot find: '{output.shorten_file_name(full_executable_name, 120)}'"
+        )
+    output.console.print()
+    if publish:
+        output.console.print(
+            ":sparkles: Debugging output from publishing datasette to fly:"
+        )
+    else:
+        output.console.print(
+            ":sparkles: Debugging output from the local datasette instance:"
+        )
+    output.console.print()
+
+
 def start_local_datasette_server(
     database_path: Path,
     datasette_metadata: Path,
@@ -108,24 +146,13 @@ def start_local_datasette_server(
     # that the output must appear here and not from the calling function
     # because once the datasette instance starts the chasten tool can
     # no longer produce output in the console
-    output.console.print()
-    output.console.print(":sparkles: Details about datasette startup:")
-    output.console.print(
-        f"{constants.markers.Indent}{small_bullet_unicode} Venv: '{output.shorten_file_name(str(virtual_env_location), 120)}'"
-    )
-    if executable_path:
-        output.console.print(
-            f"{constants.markers.Indent}{small_bullet_unicode} Program: '{output.shorten_file_name(executable_path, 120)}'"
-        )
+    if publish:
+        label = ":sparkles: Details for datasette publishing:"
     else:
-        output.console.print(
-            f"{constants.markers.Indent}{small_bullet_unicode} Cannot find: '{output.shorten_file_name(full_executable_name, 120)}'"
-        )
-    output.console.print()
-    output.console.print(
-        ":sparkles: Debugging output from the local datasette instance:"
-    )
-    output.console.print()
+        label = ":sparkles: Details for datasette startup:"
+    display_datasette_details(label, virtual_env_location, str(executable_path), full_executable_name, publish)
+
+
     # run the localhost server
     if not publish:
         # the metadata parameter should not be passed to the datasette
@@ -140,7 +167,12 @@ def start_local_datasette_server(
                 str(datasette_port),
             ]
         else:
-            cmd = [str(full_executable_name), str(database_path), "-p", str(datasette_port)]
+            cmd = [
+                str(full_executable_name),
+                str(database_path),
+                "-p",
+                str(datasette_port),
+            ]
         # run the datasette server as a subprocess of chasten;
         # note that the only way to stop the server is to press CTRL-C;
         # there is debugging output in the console to indicate this option.
