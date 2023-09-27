7d413f073b7d0045cde9c8502f749cecd2000569
Poiuy7312 <pjs075637@gmail.com>
2023-09-27 09:52:01 -0400

    fix: removed pytest fuzzer for test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,11 +1,8 @@
 """Pytest test suite for the database module."""
 
-import pytest
-
 from chasten import database, filesystem, util
 
 
-@pytest.mark.fuzz
 def test_executable_name() -> None:
     assert filesystem.can_find_executable(
         database.executable_name(OpSystem=util.get_OS())
