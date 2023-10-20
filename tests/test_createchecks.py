1ec0f353f9a13e132e29fbff84200b44e506c496
Alish Chhetri <chhetri01@cslaptop.localdomain>
2023-10-20 11:21:10 -0400

    fix: reworked createchecks test

--- tests/test_createchecks.py
+++ tests/test_createchecks.py
@@ -1,53 +1,43 @@
-import openai
 import pytest
-from hypothesis import given, settings, strategies as st
-from chasten.createchecks import generate_key, encrypt_key, decrypt_key, is_valid_api_key, get_user_api_key, load_user_api_key, generate_yaml_config
+from hypothesis import given
+from hypothesis import strategies as st
 from pathlib import Path
+import os
 
+from chasten.createchecks import generate_key, encrypt_key, decrypt_key, is_valid_api_key, get_user_api_key, load_user_api_key
 
-API_KEY_FILE = "userapikey.txt"
-
-valid_api_key = "your_valid_api_key_here"
-invalid_api_key = "your_invalid_api_key_here"
+# Define a valid API key for testing
+VALID_API_KEY = "your_api_key_here"  # Replace with a valid API key for testing
 
 @pytest.fixture
-def setup_api_key_file():
-    get_user_api_key(valid_api_key)
-    yield
+def key():
+    return generate_key()
 
-    with open(API_KEY_FILE, "w") as f:
-        f.write("")
-
-def test_generate_key():
-    key = generate_key()
-    assert key is not None
-    assert isinstance(key, bytes)
-
-def test_encrypt_decrypt_key():
-    user_api_key = "your_api_key_here"
-    key = generate_key()
-    encrypted_key = encrypt_key(user_api_key, key)
-    decrypted_key = decrypt_key(encrypted_key, key)
-    assert user_api_key == decrypted_key
-
-def test_is_valid_api_key():
-    assert is_valid_api_key(valid_api_key)
-    assert not is_valid_api_key(invalid_api_key)
+@pytest.fixture
+def invalid_key():
+    return "invalid_key"  # A known invalid key for testing
 
 @given(st.text())
-@settings(max_examples=10)
-def test_generate_key_property(input_string):
-    key = generate_key()
-    assert len(key) == 32
+def test_encrypt_decrypt_key(key, text):
+    encrypted_key = encrypt_key(VALID_API_KEY, key)
+    decrypted_key = decrypt_key(encrypted_key, key)
+    assert decrypted_key == VALID_API_KEY
 
-def test_load_user_api_key(setup_api_key_file):
-    loaded_user_api_key = load_user_api_key(API_KEY_FILE)
-    assert loaded_user_api_key == valid_api_key
+def test_generate_key(key):
+    assert key is not None
 
-def test_generate_yaml_config_creates_file():
-    user_api_key = "your_valid_api_key_here"
-    user_input = "Your test user input"
+def test_is_valid_api_key():
+    assert is_valid_api_key(VALID_API_KEY) is True
+    assert is_valid_api_key("invalid_key") is False
 
-    generated_yaml = generate_yaml_config(user_api_key, user_input)
+def test_get_user_api_key(key):
+    get_user_api_key(VALID_API_KEY)
+    loaded_api_key = load_user_api_key("userapikey.txt")
+    assert loaded_api_key == VALID_API_KEY
 
-    assert Path("checks.yml").exists()
+def test_files_exist():
+    assert os.path.exists("userapikey.txt")
+    assert os.path.exists("checks.yml")
+    # Clean up files after testing
+    os.remove("userapikey.txt")
+    os.remove("checks.yml")
