65af1f1155f4b6e15f98ddbd47282952c95838f4
AlishChhetri <chhetri01@allegheny.edu>
2023-11-10 10:28:51 -0500

    feat: test is_valid_api_key using a valid key

--- tests/test_createchecks.py
+++ tests/test_createchecks.py
@@ -6,3 +6,12 @@ from chasten.createchecks import is_valid_api_key, generate_yaml_config
 valid_api_key = os.getenv("API_KEY")
 test_genscript = "Write: 'Hello, World'"
 file_path = "test_checks.yml"
+
+def test_valid_api_key():
+    """Test is_valid_api_key function with a valid api key."""
+    global valid_api_key
+    if not valid_api_key:
+        pytest.skip("No valid API key found in the environment variables")
+
+    result = is_valid_api_key(valid_api_key)
+    assert result is True
