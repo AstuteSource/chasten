3216e1448da5e8ef3c55e6bb4105f4c23220bf37
AlishChhetri <chhetri01@allegheny.edu>
2023-11-10 10:27:48 -0500

    feat: set global variables for test_createchecks

--- tests/test_createchecks.py
+++ tests/test_createchecks.py
@@ -2,3 +2,7 @@ import os
 import pytest
 from pathlib import Path
 from chasten.createchecks import is_valid_api_key, generate_yaml_config
+
+valid_api_key = os.getenv("API_KEY")
+test_genscript = "Write: 'Hello, World'"
+file_path = "test_checks.yml"
