4a2d89a3274abe8ccb0351cd87fd09fee98915fa
AlishChhetri <chhetri01@allegheny.edu>
2023-11-10 10:31:08 -0500

    feat: test generate_yaml_config by checking for existence of input in the output file

--- tests/test_createchecks.py
+++ tests/test_createchecks.py
@@ -7,6 +7,7 @@ valid_api_key = os.getenv("API_KEY")
 test_genscript = "Write: 'Hello, World'"
 file_path = "test_checks.yml"
 
+
 def test_valid_api_key():
     """Test is_valid_api_key function with a valid api key."""
     global valid_api_key
@@ -16,8 +17,28 @@ def test_valid_api_key():
     result = is_valid_api_key(valid_api_key)
     assert result is True
 
+
 def test_invalid_api_key():
     """Test is_valid_api_key function with an invalid api key."""
     invalid_api_key = "fk-561sf56a1sf561as5f1asf165as1"
     result = is_valid_api_key(invalid_api_key)
     assert result is False
+
+
+def test_generate_yaml_config():
+    global valid_api_key, test_genscript, file_path
+
+    if not valid_api_key:
+        pytest.skip("No valid API key found in the environment variables")
+
+    file_path = Path(file_path)
+
+    result = generate_yaml_config(file_path, valid_api_key, test_genscript)
+
+    assert result is not None 
+    assert file_path.is_file()
+
+    # Check the content of the generated file
+    with open(file_path, "r") as f:
+        content = f.read()
+        assert "Hello, World" in content
