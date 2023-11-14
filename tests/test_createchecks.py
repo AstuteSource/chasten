4f01018e375e1d26eb8b4324e4932ec0f822f591
AlishChhetri <chhetri01@allegheny.edu>
2023-11-14 15:13:08 -0500

    feat: add api test into workflow

--- tests/test_createchecks.py
+++ tests/test_createchecks.py
@@ -3,7 +3,7 @@ import pytest
 from pathlib import Path
 from chasten.createchecks import is_valid_api_key, generate_yaml_config
 
-valid_api_key = os.getenv("API_KEY")
+valid_api_key = os.getenv("OPEN_AI_KEY")
 test_genscript = "Write: 'Hello, World'"
 file_path = "test_checks.yml"
 
