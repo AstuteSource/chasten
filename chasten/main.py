afba7fcf2f103724bab8593c6611af102388856f
boulais01 <bookdragon1202@gmail.com>
2023-11-21 15:15:19 -0600

    fix: linting types

--- chasten/main.py
+++ chasten/main.py
@@ -595,7 +595,8 @@ def analyze(  # noqa: PLR0912, PLR0913, PLR0915
         output.console.print(":memo: Saving XML...")
         if os.path.isdir(input_path):
             for each_file in os.listdir(input_path):
-                each_file = Path(input_path) / Path(each_file)
+                Path(each_file)
+                each_file = Path(input_path) / each_file
                 if (
                     not os.path.isdir(each_file)
                     and os.path.isfile(each_file)
@@ -616,7 +617,8 @@ def analyze(  # noqa: PLR0912, PLR0913, PLR0915
                 elif os.path.isdir(each_file):
                     for sub_file in os.listdir(each_file):
                         if str(sub_file).endswith(".py"):
-                            sub_file = Path(each_file) / Path(sub_file)
+                            Path(sub_file)
+                            sub_file = Path(each_file) / sub_file
                             contents = Path(sub_file).read_bytes()
                             _, ast = pyastgrep.files.parse_python_file(
                                 contents, sub_file, auto_dedent=False
@@ -643,7 +645,8 @@ def analyze(  # noqa: PLR0912, PLR0913, PLR0915
         output.console.print(":memo: Viewing XML...")
         if os.path.isdir(input_path):
             for each_file in os.listdir(input_path):
-                each_file = Path(input_path) / Path(each_file)
+                Path(each_file)
+                each_file = Path(input_path) / each_file
                 if (
                     not os.path.isdir(each_file)
                     and os.path.isfile(each_file)
@@ -672,7 +675,8 @@ def analyze(  # noqa: PLR0912, PLR0913, PLR0915
                     )
                 elif os.path.isdir(each_file):
                     for sub_file in os.listdir(each_file):
-                        sub_file = Path(each_file) / Path(sub_file)
+                        Path(sub_file)
+                        sub_file = Path(each_file) / sub_file
                         if str(sub_file).endswith(".py"):
                             contents = Path(sub_file).read_bytes()
                             _, ast = pyastgrep.files.parse_python_file(
