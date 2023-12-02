1e054efbe1957b23243459d16f29a511c4e177f4
Caleb <calebkendra7@gmail.com>
2023-12-01 21:30:33 -0500

    added test_display_detail_publish_tree main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -532,3 +532,23 @@ def test_analyze_store_results_valid_path(directory, cwd):
         ],
     )
     assert result.exit_code == 0
+
+
+@given(
+    label=strategies.characters(),
+    database_path=strategies.builds(Path),
+    metadata=strategies.builds(Path),
+    publish=strategies.booleans()
+)
+@pytest.mark.fuzz
+def test_display_detail_publish_true(
+    label: str,
+    database_path: Path,
+    metadata: Path,
+    publish: bool,
+) -> None:
+    """Confirm that the function does not crash."""
+    main.display_serve_or_publish_details(
+        label=label, database_path=database_path, metadata=metadata, publish=publish
+        )
+
